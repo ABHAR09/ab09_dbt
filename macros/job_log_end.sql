a087f2e9fc94dd0f900785f181dd0270a4535a8c
ABHAR09 <bharath.a@pfizer.com>
2024-03-26 14:37:09 +0530

    updated job_log_End

--- macros/job_log_end.sql
+++ macros/job_log_end.sql
@@ -6,16 +6,13 @@ Description: Used to update the job_run_log table once the model run is done
     {% if execute %}
         {% for res in results %}
         {%set run_dict = res.to_dict()%}
-        /* Feching job status */
         {% set job_status = run_dict.get('status') %}
         {% if "SUCCESS" not in res.message %}
                 {% set log_message = res.message %}
         {% else %}
                 {% set log_message = 'null' %}
         {% endif %}
-        /* Feching model name*/
         {%set model_nm = run_dict.get('node').get('name')%}
-        /* Feching source count*/
         {% set source_data %}
         select count(*) from {{source(run_dict.node.sources[3][0],run_dict.node.sources[3][1])}};
         {% endset %}
@@ -23,7 +20,6 @@ Description: Used to update the job_run_log table once the model run is done
         {% if execute %}
         {% set source_count = source_results.columns[0].values()[0] %}
         {% endif %}
-        /* Feching job info */
         {%set job_info %}
         select job_run_id, job_id,batch_run_id from {{source('AUDIT','JOB_RUN_LOG')}}
         WHERE JOB_ID = (
@@ -36,7 +32,6 @@ Description: Used to update the job_run_log table once the model run is done
         {% set job_id = run_job_info.columns[1].values()[0] %}
         {% set batch_run_id = run_job_info.columns[2].values()[0] %}
         {% endif %}
-        /* updating the job log run table */
         {% set update_query %}
         update {{ source('AUDIT', 'JOB_RUN_LOG') }} 
         set 
@@ -48,7 +43,6 @@ Description: Used to update the job_run_log table once the model run is done
         where job_id = {{job_id}} and job_run_id  = {{job_run_id}} 
         {% endset %}
         {% do run_query(update_query) %}
-        /* Updating the batch run log table if the model gets failed */
         {% set quary_upd_failed %}
             UPDATE
                {{source('AUDIT','BATCH_RUN_LOG')}}
